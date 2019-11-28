67e15034c3a2f7a200a39d0c4784956bdff9f5a7
a6hinavram <57791254+a6hinavram@users.noreply.github.com>
2019-11-28 20:40:24 +0530

    WSDL Update

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,37 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::any('server', function () {
+    ini_set( "soap.wsdl_cache_enabled", 0 );
+    ini_set( 'soap.wsdl_cache_ttl', 0 );
+    function login( $login, $password )
+    {
+        $credentials = [
+            'email' => $login,
+            'password' => $password
+        ];
+
+        if (auth()->attempt($credentials)) {
+            $token = auth()->user()->createToken('Test')->accessToken;
+            return response()->json(['token' => $token], 200);
+        } else {
+            return response()->json(['error' => 'UnAuthorised'], 401);
+        }
+    }
+
+    $server = new SoapServer( "http://192.168.33.10/data4/dials_activity/public/dialsactivity.wsdl" );
+    $server->addFunction( "login" );
+    $server->handle();
+});
+
+Route::get('client', function() {
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
+    $client = new \SoapClient("http://192.168.33.10/data4/dials_activity/public/dialsactivity.wsdl", array('cache_wsdl' => WSDL_CACHE_NONE));
+    return $client->login('satya@test.com', 'satya@test.com'); // call login() from .wsdl
+});
+
+
